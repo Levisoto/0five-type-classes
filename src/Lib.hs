69e4dfdb2376cc13a01c6dbdbd45ab62479307c1
Levisoto <levisoto.337@gmail.com>
2017-12-02 15:25:02 -0500

    I already have completed the problem 3

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,19 +4,48 @@ module Lib where
 
 import ExprT
 import Parser
--- import StackVM
+import StackVM
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+-------------------------------------------------------------------------
+  -- problem 1
+-------------------------------------------------------------------------
+
 eval :: ExprT -> Integer
-eval (Lit x) = x
-eval (Add x y) = (eval x) + (eval y)
-eval (Mul x y) = (eval x) * (eval y)
+eval (ExprT.Lit x) = x
+eval (ExprT.Add x y) = (eval x) + (eval y)
+eval (ExprT.Mul x y) = (eval x) * (eval y)
+
+-------------------------------------------------------------------------
+
+-------------------------------------------------------------------------
+  -- problem 2
+-------------------------------------------------------------------------
 
 evalStr :: String -> Maybe Integer
 evalStr xs 
   | val /= Nothing = fmap eval val
   | otherwise = Nothing
     where
-      val = parseExp Lit Add Mul xs
+      val = parseExp ExprT.Lit ExprT.Add ExprT.Mul xs
+
+-------------------------------------------------------------------------
+
+-------------------------------------------------------------------------
+  -- problem 3
+-------------------------------------------------------------------------
+
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = ExprT.Lit
+  add = ExprT.Add
+  mul = ExprT.Mul
+
+reify :: ExprT -> ExprT
+reify = id
