8083de9708c07ec52643fc34b9181db020db72c4
Levisoto <levisoto.337@gmail.com>
2017-12-05 23:19:45 -0500

    I already have complete the problem 4. Adding test to the app file

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,12 +2,19 @@ module Lib where
     -- ( someFunc
     -- ) where
 
-import ExprT
+import ExprT (ExprT (..))
 import Parser
-import StackVM
+--import StackVM
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  putStrLn "test of problem 4\n=================="
+  print testInteger
+  print testBool
+  print testMM
+  print testSat
+  putStrLn "===================\n"
+
 
 -------------------------------------------------------------------------
   -- problem 1
@@ -49,3 +56,40 @@ instance Expr ExprT where
 
 reify :: ExprT -> ExprT
 reify = id
+
+-------------------------------------------------------------------------
+
+-------------------------------------------------------------------------
+  -- problem 4
+-------------------------------------------------------------------------
+
+instance Expr Integer where
+  lit x = x
+  add x y = (+) x y
+  mul x y = (*) x y
+
+instance Expr Bool where
+  lit x = (>) x 0
+  add x y = x || y
+  mul x y = x && y
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit x = MinMax x
+  add (MinMax x) (MinMax y)= MinMax $ max x y
+  mul (MinMax x) (MinMax y)= MinMax $ min x y
+
+instance Expr Mod7 where
+  lit x = Mod7 $ if (x<=0) then 0 else mod x 7
+  add (Mod7 x) (Mod7 y) = lit $ x + y
+  mul (Mod7 x) (Mod7 y) = lit $ x * y
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
